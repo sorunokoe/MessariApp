6c415398943cbdd303bc1b5bd2572fa923f28409
Salgara <sorunokoe@gmail.com>
2020-12-25 01:28:44 +0600

    Initial

---
+++ MessariApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessariApp
+//
+//  Created by Yeskendir Salgara on 25.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# MessariApp
+
+## iOS App - alternative interface for Messari API
+
+Swift 5, iOS:14.2
+
+### API
+```https://messari.io/api/docs```
+
+### Build
+```pod update```
+
+
+
